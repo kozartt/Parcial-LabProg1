659a93b702a72a4eea81a65c8007fe84ca45071a
Andreacg <andreacgp96@gmail.com>
2016-09-27 21:39:14 -0300

    Add files via upload

---
+++ ASD/src/dsadsa/Dsadsa.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * 1- calcular el perimetro del triangulo
+   2- calcular la superficie del triangulo
+   3- calcular perimetro del cuadrado
+   4- calcular perimetro del cuadrado
+ */ 
+package dsadsa;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Puesto 1.6
+ */
+public class Dsadsa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int numeroSw;
+    
+    String cadenaNumeroSw;
+    
+    cadenaNumeroSw=JOptionPane.showInputDialog("MENU\n1-Calcular el perimetro del triangulo"
+                        + "\n2 - Calcular superficie del triangulo"
+                        + "\n3 - Calcular perimetro del cuadrado"
+                        + "\n4 - Calcular superficie del cuadrado"
+                        + "\n5 - Calcular perímetro del pentágono"
+                        + "\n6 - Salir");
+                    
+    
+    }
+    
+}
